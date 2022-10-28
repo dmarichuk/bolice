4ded0d39d7decb3e1b906c261997d8c4097e0892
Daniil Marichuk <danmarichuk@gmail.com>
2022-10-28 09:20:50 +0300

    парсинг фоток чата

--- app/bot.py
+++ app/bot.py
@@ -1,7 +1,9 @@
 import asyncio
 import uvloop
+import os
 
 from db import MongoConnection
+from pymongo import errors as mongo_errors
 from config import (TELEGRAM_API_ID, TELEGRAM_API_HASH, TELEGRAM_BOT_TOKEN)
 from pyrogram import Client, filters
 from hash import init_image, get_phash
@@ -11,19 +13,24 @@ from utils import get_custom_logger
 logger = get_custom_logger("bot")
 uvloop.install()
 
-app = Client(
-    "agressive_d_disk_bot",
+user_app = Client(
+    "bolice_user",
     api_id=TELEGRAM_API_ID, 
-    api_hash=TELEGRAM_API_HASH, 
+    api_hash=TELEGRAM_API_HASH
+)
+
+bot_app = Client(
+    "bolice_bot",
+    api_id=TELEGRAM_API_ID,
+    api_hash=TELEGRAM_API_HASH,
     bot_token=TELEGRAM_BOT_TOKEN,
     in_memory=True
 )
 
-
-@app.on_message(filters.photo)
+@bot_app.on_message(filters.photo)
 async def photo_handler(client, message): 
     f = await client.download_media(message.photo, in_memory=True)
-    logger.info(f"Recieved photo from user {message.from_user.id} in chat {message.chat.id}")
+    # logger.info(f"Recieved photo from user {message.from_user.id} in chat {message.chat.id}")
     
     img = init_image(f)
     if not img:
@@ -34,10 +41,37 @@ async def photo_handler(client, message):
     hash = get_phash(img)
     logger.info(f"Obtained image hash: {str(hash)}")
 
-    conn = MongoConnection()
-    col = conn[message.chat.id]
-    doc = col.insert_one({"img_hash": str(hash), "message_id": message.id, "active": True})
-    logger.info(f"Inserted document {doc} to db")
+    # conn = MongoConnection()
+    # col = conn[message.chat.id]
+    # doc = col.insert_one({"img_hash": str(hash), "message_id": message.id, "active": True})
+    # logger.info(f"Inserted document {doc} to db")
+
     
+async def parse_chat_photos(client, chat_id):
+    conn = MongoConnection()
+    col = conn[str(chat_id)]
+
+    async with client:
+        async for msg in client.get_chat_history(chat_id):
+            if msg.photo:
+                f = await client.download_media(msg.photo, in_memory=True)
+                img = init_image(f)
+                hash = get_phash(img)
+                try:
+                    doc = col.insert_one({"img_hash": str(hash), "message_id": msg.id, "file_id": msg.photo.file_id, "active": True})
+                    logger.info(f"Inserted document {doc} to db")
+                except mongo_errors.DuplicateKeyError:
+                    logger.warning("Hash already in DB")
+
+                    logger.info("Getting duplicate image")
+                    dup_doc = col.find_one({"img_hash": str(hash)})
+                    
+                    logger.info("Creating directory and save collisions")
+                    dir_name = f'{msg.id}_{dup_doc["message_id"]}'
+                    await client.download_media(msg.photo, f"./media/collisions/{dir_name}/")
+                    await client.download_media(dup_doc["file_id"], f"./media/collisions/{dir_name}/")
+                
+
 if __name__ == "__main__":
-    app.run()    
+    # bot_app.run()
+    user_app.run(parse_chat_photos(user_app, -1001253753634))
